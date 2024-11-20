8a0c01060ecb856cb02232181042eeabe47c6190
david <david@test.com>
2024-11-19 22:36:26 -0400

    dinamic forms and validations

--- src/app/feature/usuario/usuario-form/usuario-form.component.ts
+++ src/app/feature/usuario/usuario-form/usuario-form.component.ts
@@ -1,21 +1,24 @@
 import { CommonModule, UpperCasePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UsuarioService } from '../../../core/service/usuario.service';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
 import { ButtonModule } from 'primeng/button';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 @Component({
   selector: 'app-usuario-form',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, FormsModule, InputTextModule, PasswordModule, ButtonModule],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, InputTextModule, PasswordModule, ButtonModule, ToastModule],
+  providers: [MessageService],
   templateUrl: './usuario-form.component.html',
   styleUrl: './usuario-form.component.scss'
 })
 export class UsuarioFormComponent implements OnInit{
 
   usuariosForm: FormGroup;
-  constructor(private formBuilder: FormBuilder, private usuariosService: UsuarioService){
+  constructor(private formBuilder: FormBuilder, private usuariosService: UsuarioService, private messageService: MessageService){
     this.usuariosForm = this.formBuilder.group({
       nombres: ['' as string, Validators.required],
       apellidos: ['', [Validators.required]],
@@ -24,8 +27,56 @@ export class UsuarioFormComponent implements OnInit{
       username: ['', Validators.required],
       razonSocial: ['', Validators.required],
       nit: ['', Validators.required],
-      direcciones: this.formBuilder.array([])
+      direcciones: this.formBuilder.array([this.formBuilder.group({
+        zona: ['', Validators.required],
+        calle: ['', Validators.required],
+        nro: ['', Validators.required]
+      })])
     })
+  }
+
+  get direcciones() {
+    return this.usuariosForm.get('direcciones') as FormArray;
+  }
+
+  isValidFormField(field: string): boolean | null {
+    return this.usuariosForm.controls[field].errors && 
+    this.usuariosForm.controls[field].touched
+  }
+
+  isValidFormFieldArray(formArray: FormArray, index: number, field: string){
+    const formGroup = formArray.controls[index] as FormGroup;
+    return formGroup.controls[field].errors && 
+    formGroup.controls[field].touched
+  }
+
+  getFormFieldErrors(field: string){
+    const errors = this.usuariosForm.controls[field].errors;
+    if(errors){
+      for(const key of Object.keys(errors)){
+        switch(key){
+          case 'required':
+            return 'Campo requerido';
+          case 'email':
+            return 'El correo debe tener un formato válido'
+        }
+      }
+    }
+    return null;
+  }
+
+  createDireccion() {
+    this.direcciones.push(
+      this.formBuilder.group({
+        zona: ['', Validators.required],
+        calle: ['', Validators.required],
+        nro: ['', Validators.required]
+      })
+    )
+  }
+
+  removeDireccion(index: number){
+    this.direcciones.removeAt(index);
   }
 
   ngOnInit(): void {
@@ -39,6 +90,8 @@ export class UsuarioFormComponent implements OnInit{
       this.usuariosService.createUser(this.usuariosForm.value).subscribe({
         next: res => console.log(res)
       })
+    }else {
+      this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Error login' })
     }
   }
 }
